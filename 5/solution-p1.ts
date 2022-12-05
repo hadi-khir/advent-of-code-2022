5de58650936ea4083332907ab1d61ea96af092b8
Hadi Khir <hadi.khir@sap.com>
2022-12-05 09:23:59 -0500

    day 5

---
+++ 5/solution-p1.ts
@@ -0,0 +1,49 @@
+import { readFileSync } from 'fs';
+
+/**
+    [B]             [B] [S]        
+    [M]             [P] [L] [B] [J]
+    [D]     [R]     [V] [D] [Q] [D]
+    [T] [R] [Z]     [H] [H] [G] [C]
+    [P] [W] [J] [B] [J] [F] [J] [S]
+[N] [S] [Z] [V] [M] [N] [Z] [F] [M]
+[W] [Z] [H] [D] [H] [G] [Q] [S] [W]
+[B] [L] [Q] [W] [S] [L] [J] [W] [Z]
+ 1   2   3   4   5   6   7   8   9 
+
+ */
+
+ const crates: any =[
+    ['B', 'W', 'N'], 
+    ['L', 'Z', 'S', 'P', 'T', 'D', 'M', 'B'], 
+    ['Q', 'H', 'Z', 'W', 'R'],
+    ['W', 'D', 'V', 'J', 'Z', 'R'],
+    ['S', 'H', 'M', 'B'],
+    ['L', 'G', 'N', 'J', 'H', 'V', 'P', 'B'],
+    ['J', 'Q', 'Z', 'F', 'H', 'D', 'L', 'S'],
+    ['W', 'S', 'F', 'J', 'G', 'Q', 'B'],
+    ['Z', 'W', 'M', 'S', 'C', 'D', 'J']
+ ];
+
+// read the input file
+const instructions = require('fs').readFileSync('input.txt', 'utf8').split('\n');
+
+for (let instruction of instructions) {
+
+    let numbers = instruction.match( /\d+/g);
+
+    const quantity = numbers[0];
+    const cratesOrigin = numbers[1];
+    const cratesDestination = numbers[2];
+
+    // move the crates one at a time
+    for (let i = 0; i < quantity; i++) {
+        const crate = crates[cratesOrigin - 1].pop();
+        crates[cratesDestination - 1].push(crate);
+    }
+}
+
+// output the top item in each crate
+crates.forEach((crate: string | any[]) => {
+    console.log(crate[crate.length - 1]);
+});
