a50ab4cbb05cda0a5db00dc93b8712466c15c6aa
Hadi Khir <hadi.khir@sap.com>
2022-12-07 15:54:10 -0500

    day 7

--- 7/solution-p2.ts
+++ 7/solution-p2.ts
@@ -2,33 +2,6 @@ import * as fs from "fs";
 
 const input = fs.readFileSync("input.txt", "utf-8").trim().split("\n");
 
-// const input = `
-// $ cd /
-// $ ls
-// dir a
-// 14848514 b.txt
-// 8504156 c.dat
-// dir d
-// $ cd a
-// $ ls
-// dir e
-// 29116 f
-// 2557 g
-// 62596 h.lst
-// $ cd e
-// $ ls
-// 584 i
-// $ cd ..
-// $ cd ..
-// $ cd d
-// $ ls
-// 4060174 j
-// 8033020 d.log
-// 5626152 d.ext
-// 7214296 k
-// `
-//     .trim()
-//     .split("\n");
 class file {
     name: string;
     size: number;
